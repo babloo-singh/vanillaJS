1c18c6c900a81faf1101f39b9473b4d30b7c8525
Babloo Singh <v-babloo@cpaperless.com>
2019-09-28 21:39:51 +0530

    keyboard-musics project

---
+++ sound-project/index.js
@@ -0,0 +1,20 @@
+const keys = Array.from(document.querySelectorAll('.key'));
+const header = document.querySelector('.heading');
+keys.forEach(key => {
+  key.addEventListener('transitionend', function(event) {
+    if (event.propertyName !== 'transform') return;
+    event.target.classList.remove('playing');
+    header.classList.remove('white');
+  });
+});
+window.addEventListener('keypress', function(event) {
+  let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  this.document.querySelector('body').style.backgroundColor = `#${randomColor}`;
+  if (!audio) return;
+  audio.currentTime = 0;
+  key.classList.add('playing');
+  header.classList.add('white');
+  audio.play();
+});
