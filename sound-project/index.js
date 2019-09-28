f8f0fcbf8303c97a89eba0db1e2032e72bef0643
Babloo Singh <v-babloo@cpaperless.com>
2019-09-28 21:42:40 +0530

    refactor

--- sound-project/index.js
+++ sound-project/index.js
@@ -10,11 +10,11 @@ keys.forEach(key => {
 window.addEventListener('keypress', function(event) {
   let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
   let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
-  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  this.document.querySelector('body').style.backgroundColor = `#${randomColor}`;
   if (!audio) return;
   audio.currentTime = 0;
   key.classList.add('playing');
   header.classList.add('white');
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  this.document.querySelector('body').style.backgroundColor = `#${randomColor}`;
   audio.play();
 });
